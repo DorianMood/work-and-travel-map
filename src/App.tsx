d4fd213670506899460fef78c72e5e7ad0b3fc3f
nekithockey75 <nikita01051997@gmail.com>
2021-01-13 23:57:56 +0500

    Set up project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Map, { IJob } from "./Map";
 
 function App() {
+  const [data, setData] = useState<IJob[]>([
+    {
+      name: "Test job",
+      link: "https://ya.ru",
+      tipped: true,
+      location: "Boston MA",
+      position: [50, 50],
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Map data={data} />
     </div>
   );
 }
---
+++ src/Map.tsx
@@ -0,0 +1,37 @@
+import { LatLngTuple } from "leaflet";
+import * as React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+export interface IJob {
+  name: string;
+  location: string;
+  tipped: boolean;
+  link: string;
+  position: LatLngTuple;
+}
+
+interface IMapProps {
+  data?: IJob[];
+}
+
+export default function Map(props: IMapProps) {
+  const { data } = props;
+
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <MapContainer center={[51.505, -0.09]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {data?.map((item, index) => {
+          return (
+            <Marker position={item.position} key={index}>
+              <Popup>{item.name}</Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
+    </div>
+  );
+}
