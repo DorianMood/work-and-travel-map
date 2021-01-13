a6e3a33f753ab6513d7dc983aa6489d7c8e4843a
nekithockey75 <nikita01051997@gmail.com>
2021-01-14 01:33:59 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Map, { IJob } from "./Map";
 
+const GEOCODER_API_KEY = process.env.REACT_APP_GEOCODER_API_KEY;
+const GEOCODER_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/batch";
+
 function App() {
-  const [data, setData] = useState<IJob[]>([
+  const [rawData, setRawData] = useState<IJob[]>([
     {
       name: "Test job",
       link: "https://ya.ru",
       tipped: true,
-      location: "Boston MA",
-      position: [50, 50],
+      location: "Wisc Dells, WI Midwest",
+      position: [1, 1],
     },
   ]);
+
+  const [data, setData] = useState<IJob[]>([]);
+
+  useEffect(() => {
+    const body = {
+      batch: rawData.map((item) => ({ query: item.location, country: "US" })),
+    };
+
+    const config = {
+      headers: { "Content-Type": "application/json" },
+    };
+
+    axios
+      .post(`${GEOCODER_BASE_URL}?key=${GEOCODER_API_KEY}`, body)
+      .then((response) => {
+        console.log(response);
+      });
+  }, [rawData]);
 
   return (
     <div>
